d7a72b2056a39e92c386de3494e8fbce118bde2e
Mazloom <rmazloom88@gmail.com>
2020-07-21 23:33:49 +0430

    Initial commit

---
+++ src/main/java/ir/ofoghkish/objecttracking/ObjectTrackingApplication.java
@@ -0,0 +1,13 @@
+package ir.ofoghkish.objecttracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectTrackingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObjectTrackingApplication.class, args);
+    }
+
+}

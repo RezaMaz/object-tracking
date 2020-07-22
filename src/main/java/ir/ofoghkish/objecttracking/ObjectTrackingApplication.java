fa7117812440e3156a7c5ededcf57ad3b104e993
Mazloom <rmazloom88@gmail.com>
2020-07-22 06:22:24 +0430

    include some properties about hibernate and datasource in application.properties and add dependencies in pom,xml and add annotation for auditing to Main class

--- src/main/java/ir/ofoghkish/objecttracking/ObjectTrackingApplication.java
+++ src/main/java/ir/ofoghkish/objecttracking/ObjectTrackingApplication.java
@@ -2,7 +2,15 @@ package ir.ofoghkish.objecttracking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories
+@EnableJpaAuditing
 @SpringBootApplication
 public class ObjectTrackingApplication {
 

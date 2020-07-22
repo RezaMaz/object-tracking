57ae375d5a7db3411a00b04be890adb055fa1f66
Mazloom <rmazloom88@gmail.com>
2020-07-23 00:25:22 +0430

    add editor name for createdBy and lastModifiedBy field in database to use by Hibernate entity listeners

--- src/main/java/ir/ofoghkish/objecttracking/ObjectTrackingApplication.java
+++ src/main/java/ir/ofoghkish/objecttracking/ObjectTrackingApplication.java
@@ -1,12 +1,16 @@
 package ir.ofoghkish.objecttracking;
 
+import ir.ofoghkish.objecttracking.configuration.AuditorAwareImpl;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories
-@EnableJpaAuditing(modifyOnCreate = false, auditorAwareRef = "auditorProvider")
+@EnableJpaAuditing(modifyOnCreate = false, auditorAwareRef = "auditorAware")
 @SpringBootApplication
 public class ObjectTrackingApplication {
 
@@ -14,4 +18,13 @@ public class ObjectTrackingApplication {
         SpringApplication.run(ObjectTrackingApplication.class, args);
     }
 
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return new AuditorAwareImpl();
+    }
 }
---
+++ src/main/java/ir/ofoghkish/objecttracking/configuration/AuditorAwareImpl.java
@@ -0,0 +1,13 @@
+package ir.ofoghkish.objecttracking.configuration;
+
+import org.springframework.data.domain.AuditorAware;
+
+import java.util.Optional;
+
+public class AuditorAwareImpl implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("Mazloom");
+    }
+}

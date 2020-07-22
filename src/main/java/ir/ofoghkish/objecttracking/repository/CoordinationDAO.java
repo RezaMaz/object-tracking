84c766da554b9d597a2ab79dc7642cf2a367f343
Reza <rmazloom88@gmail.com>
2020-07-22 10:35:52 +0430

    change name of class repositories to DAO named

--- src/main/java/ir/ofoghkish/objecttracking/repository/CoordinationDAO.java
+++ src/main/java/ir/ofoghkish/objecttracking/repository/CoordinationDAO.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CoordinationRepository extends JpaRepository<Coordination, Long> {
+public interface CoordinationDAO extends JpaRepository<Coordination, Long> {
 }

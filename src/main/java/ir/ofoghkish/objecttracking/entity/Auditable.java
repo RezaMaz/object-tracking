82f3ac9e45674eb9cf4e65c1f71982fcb0005b85
Reza <rmazloom88@gmail.com>
2020-07-22 10:02:55 +0430

    change auditable column names and add MappedSupperClass annotation to it for disable generate specific table for it by Hibernate

--- src/main/java/ir/ofoghkish/objecttracking/entity/Auditable.java
+++ src/main/java/ir/ofoghkish/objecttracking/entity/Auditable.java
@@ -1,5 +1,7 @@
 package ir.ofoghkish.objecttracking.entity;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -7,27 +9,35 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
-import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Version;
 import java.util.Date;
 
-@Entity
+@Getter
+@Setter
+@MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class Auditable {
 
-    @Column(name = "CREATED_DATE", nullable = false, updatable = false)
+    @Column(name = "D_CREATED_DATE", nullable = false, updatable = false)
     @CreatedDate
     private Date createdDate;
-    @Column(name = "MODIFIED_DATE")
-    @LastModifiedDate
-    private Date modifiedDate;
-    @Column(name = "CREATED_BY")
+
+    @Column(name = "C_CREATED_BY", nullable = false, updatable = false)
     @CreatedBy
     private String createdBy;
-    @Column(name = "MODIFIED_DATE")
+
+    @Column(name = "D_MODIFIED_DATE")
+    @LastModifiedDate
+    private Date modifiedDate;
+
+    @Column(name = "C_MODIFIED_BY")
     @LastModifiedBy
     private String modifiedBy;
-    @Column(name = "N_VERSION")
+
+    @Version
+    @Column(name = "N_VERSION", nullable = false)
     private Long version;
 
 }

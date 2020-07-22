35433fb2266ab7fc6bee75e70553d5674a21b552
Mazloom <rmazloom88@gmail.com>
2020-07-23 03:09:07 +0430

    set modelmapper type STRICT

--- src/main/java/ir/ofoghkish/objecttracking/ObjectTrackingApplication.java
+++ src/main/java/ir/ofoghkish/objecttracking/ObjectTrackingApplication.java
@@ -2,6 +2,7 @@ package ir.ofoghkish.objecttracking;
 
 import ir.ofoghkish.objecttracking.configuration.AuditorAwareImpl;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -20,7 +21,9 @@ public class ObjectTrackingApplication {
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
     }
 
     @Bean

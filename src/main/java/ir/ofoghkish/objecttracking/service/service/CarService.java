78fd2ad647ed69aa8b7eca33e5d3b23796593953
Reza <rmazloom88@gmail.com>
2020-07-22 11:01:35 +0430

    add service for CRUD operations for car and coordination

---
+++ src/main/java/ir/ofoghkish/objecttracking/service/service/CarService.java
@@ -0,0 +1,45 @@
+package ir.ofoghkish.objecttracking.service.service;
+
+import ir.ofoghkish.objecttracking.entity.Car;
+import ir.ofoghkish.objecttracking.repository.CarDAO;
+import ir.ofoghkish.objecttracking.service.dto.CarDTO;
+import ir.ofoghkish.objecttracking.service.exception.NotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class CarService {
+
+    private final ModelMapper modelMapper;
+    private final CarDAO carDAO;
+
+    public CarDTO.Info create(CarDTO.Create request) {
+        final Car car = modelMapper.map(request, Car.class);
+        return save(car);
+    }
+
+    public CarDTO.Info update(CarDTO.Update request) {
+        final Optional<Car> byId = carDAO.findById(request.getId());
+        final Car car = byId.orElseThrow(() -> new NotFoundException(Car.class));
+
+        Car updating = new Car();
+        modelMapper.map(car, updating);
+        modelMapper.map(request, updating);
+
+        return save(updating);
+    }
+
+    public void delete(Long id) {
+        carDAO.deleteById(id);
+    }
+
+    private CarDTO.Info save(Car car) {
+        final Car saved = carDAO.saveAndFlush(car);
+        return modelMapper.map(saved, CarDTO.Info.class);
+    }
+
+}
---
+++ src/main/java/ir/ofoghkish/objecttracking/service/service/CoordinationService.java
@@ -0,0 +1,45 @@
+package ir.ofoghkish.objecttracking.service.service;
+
+import ir.ofoghkish.objecttracking.entity.Coordination;
+import ir.ofoghkish.objecttracking.repository.CoordinationDAO;
+import ir.ofoghkish.objecttracking.service.dto.CoordinationDTO;
+import ir.ofoghkish.objecttracking.service.exception.NotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class CoordinationService {
+
+    private final ModelMapper modelMapper;
+    private final CoordinationDAO coordinationDAO;
+
+    public CoordinationDTO.Info create(CoordinationDTO.Create request) {
+        final Coordination coordination = modelMapper.map(request, Coordination.class);
+        return save(coordination);
+    }
+
+    public CoordinationDTO.Info update(CoordinationDTO.Update request) {
+        final Optional<Coordination> byId = coordinationDAO.findById(request.getId());
+        final Coordination coordination = byId.orElseThrow(() -> new NotFoundException(Coordination.class));
+
+        Coordination updating = new Coordination();
+        modelMapper.map(coordination, updating);
+        modelMapper.map(request, updating);
+
+        return save(updating);
+    }
+
+    public void delete(Long id) {
+        coordinationDAO.deleteById(id);
+    }
+
+    private CoordinationDTO.Info save(Coordination coordination) {
+        final Coordination saved = coordinationDAO.saveAndFlush(coordination);
+        return modelMapper.map(saved, CoordinationDTO.Info.class);
+    }
+
+}

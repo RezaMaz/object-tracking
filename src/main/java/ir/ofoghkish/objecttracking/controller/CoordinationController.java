6da48b98da9c5f112ed3ccd3508b89670ed55d13
Mazloom <rmazloom88@gmail.com>
2020-07-23 22:34:22 +0430

    add cross origin to rest api to request from localhost:4200 to localhost:8080

--- src/main/java/ir/ofoghkish/objecttracking/controller/CoordinationController.java
+++ src/main/java/ir/ofoghkish/objecttracking/controller/CoordinationController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/api/v1/coordination")

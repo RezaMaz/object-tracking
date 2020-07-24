44c8622d9c976aff65b99e25d7a25cdb57c96299
Mazloom <rmazloom88@gmail.com>
2020-07-24 14:38:24 +0430

    add final to class field for DI works

--- src/test/java/ir/ofoghkish/objecttracking/SomeBusinessImpl.java
+++ src/test/java/ir/ofoghkish/objecttracking/SomeBusinessImpl.java
@@ -3,7 +3,7 @@ package ir.ofoghkish.objecttracking;
 import ir.ofoghkish.objecttracking.service.iservice.ICarService;
 
 public class SomeBusinessImpl {
-    private ICarService iCarService;
+    private final ICarService iCarService;
 
     public SomeBusinessImpl(ICarService iCarService) {
         super();
